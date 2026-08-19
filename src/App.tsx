389e913b6aa0a628b8db7ef6d334ab3c65304a59
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-08-19 13:54:52 +0500

    added the debugged explore page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import './App.css'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './MyComponents/HomePage'
+import ExplorePage from './MyComponents/ExplorePage'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/ExplorePage" element={<ExplorePage />} />
+        
+
+        
       </Routes>
     </BrowserRouter>
   )
