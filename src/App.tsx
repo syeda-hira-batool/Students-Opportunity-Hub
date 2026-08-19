1db9b747780acf7968d68c8e752c378960aecc6d
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-08-19 14:40:47 +0500

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './MyComponents/HomePage'
 import ExplorePage from './MyComponents/ExplorePage'
-
+import AboutPage from './MyComponents/AboutPage'
+import InternshipsPage from './MyComponents/InternshipsPage'
+import ScholarshipsPage from './MyComponents/ScholarshipsPage'
+import FellowshipsPage from './MyComponents/FellowshipsPage'
+import CompetitionsPage from './MyComponents/CompetitionsPage'
+import HackathonPage from './MyComponents/HackathonPage'
+import ConferencesPage from './MyComponents/ConferencesPage'
+import ExchangeProgramsPage from './MyComponents/ExchangeProgramsPage'
+import SummerSchoolsPage from './MyComponents/SummerSchoolsPage'
+import BootcampsPage from './MyComponents/BootcampsPage'
+import VolunteeringPage from './MyComponents/VolunteeringPage'
+import StudentJobsPage from './MyComponents/StudentJobsPage'
 
 function App() {
   return (
@@ -11,9 +22,19 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ExplorePage" element={<ExplorePage />} />
-        
+        <Route path="/AboutPage" element={<AboutPage />} />
 
-        
+        <Route path="/InternshipsPage" element={<InternshipsPage />} />
+        <Route path="/ScholarshipsPage" element={<ScholarshipsPage />} />
+        <Route path="/FellowshipsPage" element={<FellowshipsPage />} />
+        <Route path="/CompetitionsPage" element={<CompetitionsPage />} />
+        <Route path="/HackathonPage" element={<HackathonPage />} />
+        <Route path="/ConferencesPage" element={<ConferencesPage />} />
+        <Route path="/ExchangeProgramsPage" element={<ExchangeProgramsPage />} />
+        <Route path="/SummerSchoolsPage" element={<SummerSchoolsPage />} />
+        <Route path="/BootcampsPage" element={<BootcampsPage />} />
+        <Route path="/VolunteeringPage" element={<VolunteeringPage />} />
+        <Route path="/StudentJobsPage" element={<StudentJobsPage />} />
       </Routes>
     </BrowserRouter>
   )
